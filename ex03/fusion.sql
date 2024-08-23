3493ba6b100ac5a846753800ee1a269769b48b95
macrespo42 <macrespo@student.42.fr>
2024-08-23 15:58:09 +0200

    feat: handle duplicate item and save join to customers

--- ex03/fusion.sql
+++ ex03/fusion.sql
@@ -1,3 +1,37 @@
+CREATE TABLE item_dedup (LIKE item);
+
+INSERT INTO item_dedup (product_id, category_id, category_code, brand)
+SELECT DISTINCT ON (product_id) product_id, category_id, category_code, brand
+FROM item;
+
+DROP TABLE item;
+
+ALTER TABLE item_dedup
+RENAME TO item;
+
+CREATE TABLE customers_left_joined_item (
+  event_time TIMESTAMP,
+  event_type VARCHAR(255),
+  product_id SERIAL,
+  price DECIMAL,
+  user_id SERIAL,
+  user_session VARCHAR(36),
+  item_category_id VARCHAR(255),
+  item_category_code VARCHAR(255),
+  item_brand VARCHAR(255)
+);
+
+INSERT INTO customers_left_joined_item (
+  event_time,
+  event_type,
+  product_id,
+  price,
+  user_id,
+  user_session,
+  item_category_id,
+  item_category_code,
+  item_brand
+)
 SELECT customers.event_time, 
   customers.event_type,
   customers.product_id,
@@ -9,3 +43,8 @@ SELECT customers.event_time,
   item.brand as item_brand
 FROM customers
 LEFT JOIN item ON customers.product_id = item.product_id;
+
+drop table customers;
+
+ALTER TABLE customers_left_joined_item
+RENAME TO customers;
