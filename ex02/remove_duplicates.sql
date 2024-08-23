2dc8c102b6191aa07423284765642169de4bf18c
macrespo42 <macrespo@student.42.fr>
2024-08-23 14:00:15 +0200

    feat: remove remove_duplicates with handling server sending dup

--- ex02/remove_duplicates.sql
+++ ex02/remove_duplicates.sql
@@ -1,7 +1,7 @@
 CREATE TABLE customers_tmp (LIKE customers);
 
 INSERT INTO customers_tmp (event_time, event_type, product_id, price, user_id, user_session)
-SELECT DISTINCT ON (event_time, event_type, product_id, price, user_id, user_session)
+SELECT DISTINCT ON (date_trunc('minute', event_time), event_type, product_id, price, user_id, user_session)
 event_time, event_type, product_id, price, user_id, user_session
 FROM customers;
 
